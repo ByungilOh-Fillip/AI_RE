6eef2ae1e00173f7c2a2e997827da7e0c986da22
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-07-20 20:07:41 +0900

    [chore] : Update component
    - UI, Component, Data를 추가하며 폴더 구조와 함께 빌드가 변경되었습니다.

--- UEProject/Source/AI_RE.Build.cs
+++ UEProject/Source/AI_RE.Build.cs
@@ -30,16 +30,18 @@ public class AI_RE : ModuleRules
 			"AI_RE/OBI/ThirdPerson/Variant_Combat/Animation",
 			"AI_RE/OBI/ThirdPerson/Variant_Combat/Gameplay",
 			"AI_RE/OBI/ThirdPerson/Variant_Combat/Interfaces",
-			"AI_RE/OBI/ThirdPerson/Variant_Combat/UI",
-			"AI_RE/OBI/Component/Public"
-			"AI_RE/Work/LMK/Public",
+			"AI_RE/OBI/Component/Public",
+			"AI_RE/OBI/UI/Public",
+			"AI_RE/Global/Components/Public",
+			"AI_RE/Global/Data",
+			"AI_RE/Work/LMK/Public"
 			
 		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using online features
+		// Uncomment if you are using online features 
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
